2e0b7d412e58fbee422c9266572da9a4363763f5
Jessica Stringham <jessstringham@users.noreply.github.com>
2024-10-22 14:09:18 -0400

    update angle pi trait def

--- murrelet_draw/src/livecodetypes.rs
+++ murrelet_draw/src/livecodetypes.rs
@@ -1,8 +1,7 @@
 // place to put newtypes for livecode
 
 pub mod anglepi {
-    use glam::Vec2;
-    use murrelet_common::{AnglePi, IsAngle};
+    use murrelet_common::{Angle, AnglePi, IsAngle};
     use murrelet_livecode_derive::Livecode;
 
     #[derive(Clone, Copy, Debug, Livecode)]
@@ -18,37 +17,10 @@ pub mod anglepi {
             Self(f.angle_pi())
         }
     }
-    impl IsAngle for LivecodeAnglePi {
-        fn angle_pi(&self) -> f32 {
-            self._to_angle_pi().angle_pi()
-        }
 
-        fn angle(&self) -> f32 {
-            self._to_angle_pi().angle()
-        }
-
-        fn as_angle(&self) -> murrelet_common::Angle {
-            self._to_angle_pi().as_angle()
-        }
-
-        fn as_angle_pi(&self) -> AnglePi {
-            self._to_angle_pi().as_angle_pi()
-        }
-
-        fn to_norm_dir(&self) -> Vec2 {
-            self._to_angle_pi().to_norm_dir()
-        }
-
-        fn to_mat3(&self) -> glam::Mat3 {
-            self._to_angle_pi().to_mat3()
-        }
-
-        fn perp_to_left(&self) -> murrelet_common::Angle {
-            self._to_angle_pi().perp_to_left()
-        }
-
-        fn perp_to_right(&self) -> murrelet_common::Angle {
-            self._to_angle_pi().perp_to_right()
+    impl From<LivecodeAnglePi> for Angle {
+        fn from(value: LivecodeAnglePi) -> Self {
+            value._to_angle_pi().as_angle()
         }
     }
 }
